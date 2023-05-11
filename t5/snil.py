de57e9f26183b4b7c6de8af4f419b9e5c4233d4b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-05-11 15:56:32 +0800

    add mask

--- t5/snil.py
+++ t5/snil.py
@@ -1,6 +1,9 @@
 from datasets import Dataset as HFDataset
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 from torch.utils.data import Dataset
+import os
+
+DEBUG = os.environ.get('DEBUG', 'false') == 'true'
 
 
 class SNIL_For_T5(Dataset):
@@ -8,12 +11,15 @@ class SNIL_For_T5(Dataset):
     def __init__(self, tokenizer: T5Tokenizer) -> None:
         super().__init__()
         self.dataset = HFDataset.from_file(
-            '/home/liukai/.cache/huggingface/datasets/esnli/plain_text/0.0.2/a160e6a02bbb8d828c738918dafec4e7d298782c334b5109af632fec6d779bbc/esnli-train.arrow',
-            split='train[0:1000]')
+            '/home/liukai/.cache/huggingface/datasets/esnli/plain_text/0.0.2/a160e6a02bbb8d828c738918dafec4e7d298782c334b5109af632fec6d779bbc/esnli-train.arrow',)
+        if DEBUG:
+            self.dataset = self.dataset.select(range(1000))
         self.tokenizer = tokenizer
 
         self.inputs = []
         self.labels = []
+        self.inputs_mask = []
+        self.label_mask = []
         self._build()
 
     def _build(self):
@@ -34,18 +40,23 @@ class SNIL_For_T5(Dataset):
             if i % 1000 == 0:
                 print(f"{i}/{len(self)}")
 
-        self.inputs = self.tokenizer.batch_encode_plus(
+        inputs = self.tokenizer.batch_encode_plus(
             inputs,
             padding='longest',
             max_length=self.tokenizer.model_max_length,
             truncation=True,
-            return_tensors='pt')['input_ids']
-        self.labels = self.tokenizer.batch_encode_plus(
+            return_tensors='pt')
+        self.inputs = inputs['input_ids']
+        self.inputs_mask = inputs['attention_mask']
+
+        labels = self.tokenizer.batch_encode_plus(
             labels,
             padding='longest',
             max_length=self.tokenizer.model_max_length,
             truncation=True,
-            return_tensors='pt')['input_ids']
+            return_tensors='pt')
+        self.labels = labels['input_ids']
+        self.label_mask = labels['attention_mask']
 
     def __len__(self) -> int:
         return len(self.dataset)
@@ -54,6 +65,8 @@ class SNIL_For_T5(Dataset):
         return {
             "input_ids": self.inputs[index].squeeze(),
             "labels": self.labels[index].squeeze(),
+            'attention_mask': self.inputs_mask[index].squeeze(),
+            'decoder_attention_mask': self.label_mask[index].squeeze()
         }
 
     def __iter__(self):
